c1dbd9c7fe80cfede46fb3449e79c004abf0b68e
therreth <thenry2@aceproject.space>
2023-01-11 14:54:34 -0600

    31 Introducing State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Modal from "./components/Modal";
-import Todo from "./components/Todo";
-import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
@@ -9,8 +7,6 @@ function App() {
       <Todo text="Pope"></Todo>
       <Todo></Todo>
       <Todo></Todo>
-      <Modal />
-      <Backdrop />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 function Todo(props) {
+  const [showModal, setModal] = useState(false);
+
+  function deleteHandler() {
+    setModal(true);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div>
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
